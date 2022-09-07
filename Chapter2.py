a1e6ed60c3f6a7acc4f7901287e56f48a4df1b71
ishtylerc <ishtyler@gmail.com>
2022-09-06 23:56:55 -0400

    Updates to Chapter 2

--- Chapter2.py
+++ Chapter2.py
@@ -1,8 +1,33 @@
-from re import S
+
+# name = 'Marry'
+# password = 'swordfish'
+# if name == 'Mary':
+#         print('Hello Marry')
+#         if password == 'swordfish':
+#                 print('Access granted.')
+#         else:
+#                 print('Wrong password')
+
+
+# age = 10
+# if name == 'Alice':
+#         print('Hi, Alice')
+# elif age < 12:
+#         print('Aloha, stranger')
+
+
+# name = 'Carol'
+# age = 3000
+# if name == 'Alice':
+#         print('Hi, Alice')
+# elif age < 12:
+#         print('You are not Alice, kiddo.')
+# else:
+#         print('You are neither Alice nor a little kid.')
 
 
 spam = 0
 if spam < 5:
         print('Hello, world')
         spam = spam + 1
-        
+       
