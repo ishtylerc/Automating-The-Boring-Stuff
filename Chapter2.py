e8a49cd1ae62eb2ab13c42c55754da404262b510
ishtylerc <ishtyler@gmail.com>
2022-09-10 12:20:23 -0400

    further updates 9/10/22

--- Chapter2.py
+++ Chapter2.py
@@ -50,3 +50,14 @@
 #     if name == 'your name':
 #         break
 # print('Thank you "your name"')
+
+while True: 
+        print('Who are you?')
+        name = input()
+        if name != 'Joe':
+                continue
+        print('Hello Joe. What is the password? It is a fish.')
+        password=input()
+        if password == 'swordfish':
+                break
+print('Access granted.')
