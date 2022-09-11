12bcd75da291771154f7732999577981a4ba257c
ishtylerc <ishtyler@gmail.com>
2022-09-10 22:13:30 -0400

    progress

--- Chapter2.py
+++ Chapter2.py
@@ -51,13 +51,17 @@
 #         break
 # print('Thank you "your name"')
 
-while True: 
-        print('Who are you?')
-        name = input()
-        if name != 'Joe':
-                continue
-        print('Hello Joe. What is the password? It is a fish.')
-        password=input()
-        if password == 'swordfish':
-                break
-print('Access granted.')
+
+#Example of while loop with continue/break statement
+# while True: 
+#         print('Who are you?')
+#         name = input()
+#         if name != 'Joe':
+#                 continue
+#         print('Hello Joe. What is the password? It is a fish.')
+#         password = input()
+#         if password == 'swordfish':
+#                 break
+# print('Access granted.')
+
+name = ''
