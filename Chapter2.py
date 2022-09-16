dfbdef0e081a03ced37ae3483fd060a4377a5a40
ishtylerc <ishtyler@gmail.com>
2022-09-16 11:26:53 -0400

    update

--- Chapter2.py
+++ Chapter2.py
@@ -94,3 +94,41 @@
 #     print(i)
 
 
+# Example of the random module in use
+# import random or from random import randint
+
+# for i in range(11):
+#         print(random.randint(1, 134523452345))
+
+
+# Using the System module 
+# import sys
+# from urllib import response
+# while True: 
+#         print('Type exit to exit')
+#         response = input()
+#         if response == 'exit':
+#                 sys.exit()
+#         print('You typed ' + response + '.')
+
+
+# Example of a number guessing game with numbers 1 through 
+import random
+secretNumber = random.randint(1, 20)
+print('I am thinking of a number between 1 and 20.')
+
+#Ask the player to guess 6 times.
+for guessesTaken in range(1, 7):
+        print('Take a guess.')
+        guess = int(input())
+
+        if guess < secretNumber:
+                print('Your guess is too low.')
+        elif guess > secretNumber:
+                print('Your guess is too high.')
+        else:
+                break #This condition is the correct guess
+if guess == secretNumber:
+        print(('Good job! You guessed my number in ' + str(guessesTaken) + 'guesses!'))
+else: 
+        print('Nope. The number I was thinking of was ' + str(secretNumber))
