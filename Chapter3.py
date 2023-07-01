64f15e16086f2858c36bf483bf4a86c3684f4d68
ishtylerc <ishtyler@gmail.com>
2023-07-01 00:21:10 -0400

    added a magic ball fuction to chapter3

--- Chapter3.py
+++ Chapter3.py
@@ -6,8 +6,23 @@
 # hello()    
 
 
-def intro_message(name, age, gender):
+""" def intro_message(name, age, gender):
     print('Hello, ' + name + 'you are ' + age + 'and you are a ' + gender + '.')
 
 intro_message('Alice ', '31 ', 'Female')
-intro_message('John ', '25 ', 'Male')   
+intro_message('John ', '25 ', 'Male')    """
+
+import random
+
+def magic3Ball(ballNumber):
+    if ballNumber == 1:
+        return 'It is certain'
+    elif ballNumber == 2:
+        return 'Ehh try again :/'
+    elif ballNumber == 3:
+        return 'You are out of luck'
+    
+r = random.randint(1, 3)
+fortune = magic3Ball(r)
+
+print(fortune)
